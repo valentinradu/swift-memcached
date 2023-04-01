e31b7e9ec6e0b85ef320f57e2d9f233eb5282ea0
Valentin Radu <radu.v.valentin@me.com>
2023-04-01 14:52:46 +0100

    Session -> Connection in tests

--- Tests/SwiftMemcachedTests/SwiftMemcachedTests.swift
+++ Tests/SwiftMemcachedTests/SwiftMemcachedTests.swift
@@ -8,23 +8,23 @@ final class SwiftMemcachedTests: XCTestCase {
     func testExample() async throws {
         // Initialization
         let url = URL(string: "localhost:2020")!
-        let session = MemcachedConnection(url: url)
+        let connection = MemcachedConnection(url: url)
 
         // Alternatively, with options
         // let options = MemcachedOptions(timeout: 10, retries: 3, enableCompression: true)
-        // let session = MemcachedConnection(url: url, options: options)
+        // let connection = MemcachedConnection(url: url, options: options)
 
-        // Once you have the a connection, you can send commands
-        let user = try await session.perform(.get(key: "user1", of: User.self))
-        try await session.perform(.set(key: "user1", value: user))
-        try await session.perform(.delete(key: "user1"))
+        // Once you have a connection, you can send commands
+        let user = try await connection.perform(.get(key: "user1", of: User.self))
+        try await connection.perform(.set(key: "user1", value: user))
+        try await connection.perform(.delete(key: "user1"))
 
         // For more complex cases, and to improve performance over the wire, you can use a pipeline
         var pipeline = MemcachedPipeline()
         let profileImageCommand = pipeline.enqueue(.get(key: "profile-image", of: Image.self))
         let backgroundImageCommand = pipeline.enqueue(.get(key: "background-image", of: Image.self))
         let thirdUserCommand = pipeline.enqueue(.get(key: "user3", of: User.self))
-        try await pipeline.flush(using: session)
+        try await pipeline.flush(using: connection)
 
         // Type safe responses and full flexibility over awaiting and error handling
         let (profileImage, backgroundImage) = try await (
